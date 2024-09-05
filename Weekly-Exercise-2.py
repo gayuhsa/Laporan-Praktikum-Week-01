bd06aa7590a299aae9e44cc644aa0a897b0094bf
gayuhsa <senoaji13254@gmail.com>
2024-09-05 18:29:22 +0700

    Create Weekly-Exercise-2.py

---
+++ Weekly-Exercise-2.py
@@ -0,0 +1,8 @@
+print("""\033[1mUNIVERSITAS TIDAR\033[0m
+Universitas Tidar merupakan Perguruan Tinggi Pemerintah atau Perguruan Tinggi Negeri yang telah ditetapkan berdasarkan Peraturan Presiden Republik Indonesia Nomor 23 Tahun 2014 pada tanggal 1 April 2014.
+
+\033[1mJURUSAN TEKNIK ELEKTRO, MEKATRONIKA, DAN INFORMASI\033[0m
+Jurusan Teknik Elektro, Mekatronika, dan Informasi terdiri atas tiga program studi, antara lain:
+    1. S1 Teknik Elektro (\033[3mBachelor of Electrical Engineering\033[0m)
+    2. S1 Teknologi Informasi (\033[3mBachelor of Information Technology\033[0m)
+    3. S1 Teknik Mekatronika (\033[3mBachelor of Mechatronics Engineering\033[0m)""")
